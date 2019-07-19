e00132ebe0a5dae54261575ba7ba75f8d0a0487a
lmarqs <marques.analista@gmail.com>
2019-07-18 22:01:21 -0300

    enhance api

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,3 +1,3 @@
 export default interface User {
-  email: string;
+  email?: string;
 }
--- src/models/artist/ArtistModel.ts
+++ src/models/artist/ArtistModel.ts
@@ -1,14 +1,14 @@
 import { ArtistCategoryModel } from "../artist-category";
 import { Model } from "../Model";
+import User from "../User";
 
-export interface ArtistModel extends Model {
+export interface ArtistModel extends Model, User {
   photo?: string;
   about?: string;
   artisticName?: string;
   name?: string;
   mainCategory?: ArtistCategoryModel;
   categories?: ArtistCategoryModel[];
-  email?: string;
   phoneNumber?: string;
   location?: string;
   onLine?: boolean;
