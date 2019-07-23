63460f408e0a7afa85ee86a7cbec816767f2e433
lmarqs <marques.analista@gmail.com>
2019-07-23 19:26:38 -0300

    replace constants by enum

--- src/models/artist/constants.ts
+++ src/models/artist/constants.ts
@@ -1,4 +1,6 @@
 export const ARTIST = "artist";
 
-export const ARTIST_SEARCH_BY_MAIN_CATEGORY = "artist_search_by_main_category";
-export const ARTIST_SEARCH_BY_TERM = "artist_search_by_term";
+export enum ArtistSearch {
+  BY_MAIN_CATEGORY = "artist_search_by_main_category",
+  BY_TERM = "artist_search_by_term",
+}
