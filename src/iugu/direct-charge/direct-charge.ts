940f38a570c6841d73d7ca8a5cc4588e5cb490be
Umbelino Gabriel <gabriel.umbelino@consultant.volvo.com>
2019-08-18 14:36:00 -0300

    add axios type

--- src/iugu/direct-charge/direct-charge.ts
+++ src/iugu/direct-charge/direct-charge.ts
@@ -1,9 +1,10 @@
+import { AxiosRequestConfig, AxiosResponse } from "axios";
 export interface DirectCharge {
   Request: Request;
   Response: Response;
 }
 
-interface Request {
+interface Request extends AxiosRequestConfig {
   url: "charge";
   method: "POST";
   data: {
@@ -55,53 +56,54 @@ interface Item {
   quantity: number;
 }
 
-type Response =
-  | {
-      status: 200;
-      data: {
-        error: {};
-        identification: string;
-        invoice_id: string;
-        pdf: string;
-        success: true;
-        url: string;
-      };
-    }
-  | {
-      status: 200;
-      data: {
-        error: {};
-        identification?: any;
-        invoice_id: string;
-        LR: string;
-        message: string;
-        pdf: string;
-        success: true;
-        url: string;
-      };
-    }
-  | {
-      status: 400;
-      data: {
-        errors: string;
-      };
-    }
-  | {
-      status: 422;
-      data: {
-        errors: {
-          total: string[];
+type Response = AxiosResponse &
+  (
+    | {
+        status: 200;
+        data: {
+          error: {};
+          identification: string;
+          invoice_id: string;
+          pdf: string;
+          success: true;
+          url: string;
         };
-      };
-    }
-  | {
-      status: 422;
-      data: {
-        errors: {
-          "payer.cpf_cnpj": string[];
-          "payer.name": string[];
-          "payer.address.zip_code": string[];
-          "payer.address.number": string[];
+      }
+    | {
+        status: 200;
+        data: {
+          error: {};
+          identification?: any;
+          invoice_id: string;
+          LR: string;
+          message: string;
+          pdf: string;
+          success: true;
+          url: string;
         };
-      };
-    };
+      }
+    | {
+        status: 400;
+        data: {
+          errors: string;
+        };
+      }
+    | {
+        status: 422;
+        data: {
+          errors: {
+            total: string[];
+          };
+        };
+      }
+    | {
+        status: 422;
+        data: {
+          errors: {
+            "payer.cpf_cnpj": string[];
+            "payer.name": string[];
+            "payer.address.zip_code": string[];
+            "payer.address.number": string[];
+          };
+        };
+      });
