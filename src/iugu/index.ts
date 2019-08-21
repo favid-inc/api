0d42022d8da3f274ee322ff02df7531c8060db88
lmarqs <marques.analista@gmail.com>
2019-08-21 10:13:58 -0300

    add GetInvoice

--- src/iugu/index.ts
+++ src/iugu/index.ts
@@ -1,3 +1,4 @@
 export * from "./account";
 export * from "./direct-charge";
+export * from "./invoice";
 export * from "./trigger";
---
+++ src/iugu/invoice/get-invoice.ts
@@ -0,0 +1,85 @@
+import { AxiosRequestConfig, AxiosResponse } from "axios";
+
+export interface GetInvoice {
+  Request: Request;
+  Response: Response;
+}
+
+type Request = AxiosRequestConfig & {
+  url: "/invoices/:id";
+  method: "GET";
+};
+
+type Response = AxiosResponse<void> & {
+  status: 200;
+  data: ResponseData;
+};
+
+interface ResponseData {
+  bank_slip: BankSlip;
+  commission_cents?: any;
+  commission: string;
+  created_at: string;
+  currency: string;
+  custom_variables: any[];
+  customer_id?: any;
+  discount_cents?: any;
+  discount?: any;
+  due_date: string;
+  email: string;
+  financial_return_date: string;
+  financial_return_dates: FinancialReturnDate[];
+  id: string;
+  installments?: any;
+  interest?: any;
+  items_total_cents: number;
+  items: Item[];
+  logs: any[];
+  notification_url?: any;
+  paid_at?: any;
+  refundable?: any;
+  return_url?: any;
+  secure_id: string;
+  secure_url: string;
+  status: string;
+  tax_cents?: any;
+  taxes_paid: string;
+  total_cents: number;
+  total: string;
+  updated_at: string;
+  user_id?: any;
+  variables: Variable[];
+}
+
+interface Variable {
+  id?: string;
+  value: string;
+  variable: string;
+}
+
+interface Item {
+  created_at: string;
+  description: string;
+  id: string;
+  price_cents: number;
+  price: string;
+  quantity: number;
+  updated_at: string;
+}
+
+interface BankSlip {
+  barcode_data: string;
+  barcode: string;
+  digitable_line: string;
+}
+
+interface FinancialReturnDate {
+  advanced: boolean;
+  amount: string;
+  executed_date: string;
+  id: number;
+  installment: number;
+  return_date: string;
+  status: string;
+  taxes: string;
+}
