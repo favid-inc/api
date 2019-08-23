8ac06b24928c4481d6dfbd0c9c4b7196e71869bd
lmarqs <marques.analista@gmail.com>
2019-08-23 19:30:40 -0300

    create Administrador

---
+++ src/model/administrador.ts
@@ -0,0 +1,7 @@
+import { Model } from "./model";
+
+export const ADMINISTRADOR = "Administrador";
+
+export interface Administrador extends Model {
+  email?: string;
+}
--- src/model/index.ts
+++ src/model/index.ts
@@ -1,4 +1,5 @@
-export * from "./artist";
+export * from "./administrador";
 export * from "./artist-category";
-export * from "./order";
+export * from "./artist";
 export * from "./model";
+export * from "./order";
