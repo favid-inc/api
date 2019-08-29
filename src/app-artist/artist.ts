a2fedcf7c5e26e8f3610ca8a12f8fcf988cc68ca
lmarqs <marques.analista@gmail.com>
2019-08-28 21:44:06 -0300

    enhance var

--- src/app-artist/artist.ts
+++ src/app-artist/artist.ts
@@ -1,7 +1,7 @@
 import { Artist } from "../model";
 import { Action } from "./action";
 
-export type UPLOAD_TEMP_DIR = "Artist/:userUid/temp";
+export type ARTIST_UPLOAD_DIR = "Artist/:userUid/temp";
 
 export interface ClaimProfile extends Action {
   Request: {
