d5e719fd0b7a7abc30d2b975521aa334b0b96026
lmarqs <marques.analista@gmail.com>
2019-09-14 17:31:14 -0300

    create recipient

---
+++ src/pagar-me/recipient/create-recipient.ts
@@ -0,0 +1,63 @@
+// https://docs.pagar.me/reference#criando-um-recebedor
+import { Action } from "../action";
+import { BankAccount } from "../bank-account";
+import { Recipient, RecipientTransferInterval } from "./recipient";
+
+export type CreateRecipient = Action<Request, Recipient>;
+
+interface Request {
+  url: "/1/recipients";
+  method: "POST";
+  data: RequestData;
+}
+
+type RequestData = {
+  transfer_enabled: boolean;
+  postback_url?: string;
+  anticipatable_volume_percentage?: string;
+  automatic_anticipation_enabled?: string;
+  automatic_anticipation_type?: "full" | "1025";
+  automatic_anticipation_days?: string;
+  automatic_anticipation_1025_delay?: string;
+  register_information: RegisterInformation;
+  metadata?: any;
+} & (
+  | {
+      bank_account_id: string;
+    }
+  | {
+      bank_account: BankAccount;
+    }) &
+  (RecipientTransferInterval);
+
+type RegisterInformation =
+  | {
+      type: "individual";
+      document_number: string;
+      name: string;
+      site_url: string;
+      email: string;
+      phone_numbers: Array<{
+        ddd: string;
+        number: string;
+        type: string;
+      }>;
+    }
+  | {
+      type: "corporation";
+      document_number: string;
+      company_name: string;
+      email: string;
+      site_url: string;
+      phone_numbers: Array<{
+        ddd: string;
+        number: string;
+        type: string;
+      }>;
+      managing_partners: Array<{
+        type: string;
+        document_number: string;
+        email: string;
+        name: string;
+      }>;
+    };
---
+++ src/pagar-me/recipient/recipient.ts
@@ -0,0 +1,66 @@
+// https://docs.pagar.me/reference#objeto-recebedor-1
+
+import { BankAccount } from "../bank-account";
+
+export type Recipient = {
+  object: "recipient";
+  id: "string";
+  bank_account: BankAccount;
+  transfer_enable: boolean;
+  last_transfer: string;
+  automatic_anticipation_enabled: boolean;
+  anticipatable_volume_percentage: number;
+  automatic_anticipation_type: "full" | "1025";
+  automatic_anticipation_days: string;
+  automatic_anticipation_1025_delay: number;
+  date_created: string;
+  date_updated: string;
+  postback_url: string;
+  status: "registration" | "affiliation" | "active" | "refused" | "suspended" | "blocked" | "inactive";
+  status_reason?: string;
+  metadata?: any;
+} & (RecipientTransferInterval);
+
+export type RecipientTransferInterval =
+  | {
+      transfer_interval: "daily";
+    }
+  | {
+      transfer_interval: "weekly";
+      transfer_day: 1 | 2 | 3 | 4 | 5;
+    }
+  | {
+      transfer_interval: "monthly";
+      transfer_day:
+        | 1
+        | 2
+        | 3
+        | 4
+        | 5
+        | 6
+        | 7
+        | 8
+        | 9
+        | 10
+        | 11
+        | 12
+        | 13
+        | 14
+        | 15
+        | 16
+        | 17
+        | 18
+        | 19
+        | 20
+        | 21
+        | 22
+        | 23
+        | 24
+        | 25
+        | 26
+        | 27
+        | 28
+        | 29
+        | 30
+        | 31;
+    };
