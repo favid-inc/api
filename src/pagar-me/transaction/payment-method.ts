c553782885ddbee603bb41169fa8c4df1d7410fc
lmarqs <marques.analista@gmail.com>
2019-09-15 13:45:40 -0300

    adjust TransactionPaymentMethod

--- src/pagar-me/transaction/payment-method.ts
+++ src/pagar-me/transaction/payment-method.ts
@@ -1,23 +1,12 @@
-export type TransactionPaymentMethod =
-  | {
-      payment_method: "boleto";
-      installments?: 1;
-      boleto_expiration_date?: string;
-      boleto_instructions?: string;
-    }
-  | {
-      payment_method: "credit_card";
-      card_holder_name: string;
-      card_number: string;
-      card_expiration_date: string;
-      card_cvv: string;
-      installments: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
-    }
-  | {
-      payment_method: "credit_card";
-      card_hash: string;
-    }
-  | {
-      payment_method: "credit_card";
-      card_id: string;
-    };
+export interface TransactionPaymentMethod {
+  boleto_expiration_date?: string;
+  boleto_instructions?: string;
+  card_cvv: string;
+  card_expiration_date: string;
+  card_hash: string;
+  card_holder_name: string;
+  card_id: string;
+  card_number: string;
+  installments?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+  payment_method: "boleto" | "credit_card";
+}
