fbd150cdda1953cd1bf9122b3cd019a8e2e0f043
lmarqs <marques.analista@gmail.com>
2019-09-25 21:38:16 -0300

    create PUBLIC_POLICIES

--- src/model/index.ts
+++ src/model/index.ts
@@ -4,5 +4,6 @@ export * from "./artist-follower";
 export * from "./artist-rate";
 export * from "./artist";
 export * from "./model";
-export * from "./order";
 export * from "./order-like";
+export * from "./order";
+export * from "./public-policies";
