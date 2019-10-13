7d6d84e5e4a783e78f19de5fdb98a9732b34f79a
lmarqs <marques.analista@gmail.com>
2019-10-13 18:44:12 -0300

    fix ORDER_UPLOAD_DIRECTORY

--- src/model/order.ts
+++ src/model/order.ts
@@ -4,7 +4,7 @@ import { Artist } from "./artist";
 
 export const ORDER = "Order";
 
-export const ORDER_UPLOAD_DIRECTORY = `${ORDER}/:artistUserUId/:orderId/:basename`;
+export const ORDER_UPLOAD_DIRECTORY = `${ORDER}/:artistUserUId/:orderId`;
 
 export interface Order extends Model {
   artistArtisticName?: string;
