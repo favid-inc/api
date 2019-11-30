8e208f135839127b8f8851a258018e277058e214
lmarqs <marques.analista@gmail.com>
2019-11-30 10:54:52 -0300

    refact actions

--- src/app-artist/action.ts
+++ src/app-artist/action.ts
@@ -1,14 +1,18 @@
-export type Request = {
+interface BaseRequest {
   url: string;
   params?: any;
-} & (
-    | {
-      method: "GET";
-    }
-    | {
-      method: "POST";
-      data?: any;
-    });
+}
+
+export interface GetRequest extends BaseRequest {
+  method: "GET";
+}
+
+export interface PostRequest extends BaseRequest {
+  method: "POST";
+  data?: any;
+}
+
+export type Request = PostRequest | GetRequest;
 
 export interface Action<Req extends Request, Res> {
   Request: Req;
