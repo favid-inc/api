76fedd11a0d1253903f16079590484fafc6162bf
lmarqs <marques.analista@gmail.com>
2019-12-28 23:00:38 -0300

    create app-artist/ListArtistRates

--- src/app-artist/artist-rate.ts
+++ src/app-artist/artist-rate.ts
@@ -1,13 +1,19 @@
 import { ArtistRate } from "../model";
 import { Action } from "./action";
 
+export type ListArtistRates = Action<
+  {
+    url: "/ListArtistRates";
+    method: "GET";
+  }
+>;
+
 export type DeleteArtistRate = Action<
   {
     url: "/DeleteArtistRate";
     method: "POST";
     data: {
       artistRateId: ArtistRate["id"],
-    },
-    params?: void
+    }
   }
 >;
